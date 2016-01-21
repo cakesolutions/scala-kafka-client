07ce6205efae4805a54d57f3beba55fc33f793bf
Jaakko Pallari <jaakkop@cakesolutions.net>
2016-01-21 11:07:36 +0000

    Don't wrap native Kafka consumer
    Currently there's no reason to wrap the native consumer.

--- src/main/scala/cakesolutions/kafka/KafkaConsumer.scala
+++ src/main/scala/cakesolutions/kafka/KafkaConsumer.scala
@@ -4,13 +4,15 @@ import java.util.Properties
 
 import cakesolutions.kafka.TypesafeConfigExtensions._
 import com.typesafe.config.Config
-import org.apache.kafka.clients.consumer.ConsumerRecords
+import org.apache.kafka.clients.consumer.{KafkaConsumer => JKafkaConsumer}
 import org.apache.kafka.common.serialization.Deserializer
-import org.slf4j.{Logger, LoggerFactory}
 
 object KafkaConsumer {
-  def apply[K, V](props: Properties, keyDeserializer: Deserializer[K], valueDeserializer: Deserializer[V]): KafkaConsumer[K, V] =
-    new KafkaConsumer[K, V](props, keyDeserializer, valueDeserializer)
+  def apply[K, V](props: Properties): JKafkaConsumer[K, V] =
+    new JKafkaConsumer[K, V](props)
+
+  def apply[K, V](props: Properties, keyDeserializer: Deserializer[K], valueDeserializer: Deserializer[V]): JKafkaConsumer[K, V] =
+    new JKafkaConsumer[K, V](props, keyDeserializer, valueDeserializer)
 
   def apply[K, V](keyDeserializer: Deserializer[K],
                   valueDeserializer: Deserializer[V],
@@ -18,7 +20,7 @@ object KafkaConsumer {
                   groupId: String = "test",
                   enableAutoCommit: Boolean = true,
                   autoCommitInterval: Int = 1000,
-                  sessionTimeoutMs: Int = 30000): KafkaConsumer[K, V] = {
+                  sessionTimeoutMs: Int = 30000): JKafkaConsumer[K, V] = {
 
     val props = new Properties()
     props.put("bootstrap.servers", bootstrapServers)
@@ -29,30 +31,6 @@ object KafkaConsumer {
     apply(props, keyDeserializer, valueDeserializer)
   }
 
-  def apply[K, V](config: Config, keyDeserializer: Deserializer[K], valueDeserializer: Deserializer[V]): KafkaConsumer[K, V] =
-    apply(config.toProperties, keyDeserializer, valueDeserializer)
-}
-
-class KafkaConsumer[K, V](props: Properties, keyDeserializer: Deserializer[K], valueDeserializer: Deserializer[V]) {
-
-  import org.apache.kafka.clients.consumer.{KafkaConsumer => JKafkaConsumer}
-
-  import scala.collection.JavaConversions._
-
-  private val log: Logger = LoggerFactory.getLogger(getClass)
-
-  val consumer = new JKafkaConsumer[K, V](props, keyDeserializer, valueDeserializer)
-
-  def subscribe(topics: List[String]): Unit = consumer.subscribe(topics)
-
-  def consume(timeout: Long): ConsumerRecords[K, V]  = {
-    val records = consumer.poll(timeout)
-    log.info("Got [{}]", records.count())
-    records
-  }
-
-  def close() = {
-    log.debug("Closing consumer")
-    consumer.close()
-  }
+  def apply[K, V](config: Config): JKafkaConsumer[K, V] =
+    apply(config.toProperties)
 }
