005c815d2b4a6d70ff53c64ba6369b442d6a909c
Simon Souter <simonsouter@gmail.com>
2016-01-22 17:30:20 +0000

    Fix basic test

--- src/test/scala/cakesolutions/kafka/KafkaIntSpec.scala
+++ src/test/scala/cakesolutions/kafka/KafkaIntSpec.scala
@@ -1,37 +1,43 @@
 package cakesolutions.kafka
 
-import java.util
-
 import org.apache.kafka.common.serialization.{StringDeserializer, StringSerializer}
 import org.slf4j.LoggerFactory
+
+import scala.collection.JavaConversions._
+import scala.util.Random
 
 class KafkaIntSpec extends KafkaTestServer {
 
   val log = LoggerFactory.getLogger(getClass)
 
-//  "Ka" should "test" in {
-//    val kafkaPort = kafkaServer.kafkaPort
-//    log.info("ZK:" + kafkaServer.zookeeperConnect)
-//    log.info("!!:" + kafkaServer)
-//    log.info("Kafka Port: [{}]", kafkaPort)
-//    val consumer = KafkaConsumer(new StringDeserializer(), new StringDeserializer(), bootstrapServers = "localhost:" + kafkaPort)
-//
-//    log.info("Kafka producer connecting on port: [{}]", kafkaPort)
-//    val producer = KafkaProducer(new StringSerializer(), new StringSerializer(), bootstrapServers = "localhost:" + kafkaPort)
-//    producer.send(KafkaProducerRecord("test", Some("1"), "a"))
-//    producer.send(KafkaProducerRecord("test", Some("2"), "a"))
-//    producer.flush()
-//    log.info("!!!!!!!!")
-//
-//    val a = new util.ArrayList[String]()
-//    a.add("test")
-//
-//    consumer.subscribe(a)
-//    val records = consumer.poll(20000)
-//
-//    records.count() shouldEqual 2
-//
-//    producer.close
-//    consumer.close
-//  }
+  "Ka" should "test" in {
+    val kafkaPort = kafkaServer.kafkaPort
+    val topic = randomString(5)
+    log.info("Using topic {}", topic)
+    log.info("ZK:" + kafkaServer.zookeeperConnect)
+    log.info("!!:" + kafkaServer)
+    log.info("Kafka Port: [{}]", kafkaPort)
+
+    val producer = KafkaProducer(new StringSerializer(), new StringSerializer(), bootstrapServers = "localhost:" + kafkaPort)
+
+    val consumer = KafkaConsumer(new StringDeserializer(), new StringDeserializer(), bootstrapServers = "localhost:" + kafkaPort)
+    consumer.subscribe(List(topic))
+    val records1 = consumer.poll(1000)
+    records1.count() shouldEqual 0
+
+    log.info("Kafka producer connecting on port: [{}]", kafkaPort)
+    producer.send(KafkaProducerRecord(topic, Some("1"), "a"))
+    producer.flush()
+
+    val records = consumer.poll(1000)
+
+    records.count() shouldEqual 1
+    producer.close
+    consumer.close
+  }
+
+  val random = new Random()
+
+  def randomString(length: Int): String =
+    random.alphanumeric.take(length).mkString
 }
