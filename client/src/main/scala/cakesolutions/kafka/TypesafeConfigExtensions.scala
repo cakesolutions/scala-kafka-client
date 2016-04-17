264a593136ab37092edd08154d48af8b9fc8b08d
Jaakko Pallari <jkpl@lepovirta.org>
2016-04-17 22:59:44 +0100

    Basic docs for client module

--- client/src/main/scala/cakesolutions/kafka/TypesafeConfigExtensions.scala
+++ client/src/main/scala/cakesolutions/kafka/TypesafeConfigExtensions.scala
@@ -6,15 +6,25 @@ import scala.collection.JavaConversions._
 import scala.collection.immutable._
 import scala.collection.mutable
 
+/**
+  * Extensions added to Typesafe [[Config]] class.
+  */
 object TypesafeConfigExtensions {
 
   implicit class RichConfig(val config: Config) extends AnyVal {
+
+    /**
+      * Convert Typesafe [[Config]] to [[Properties]].
+      */
     def toProperties: Properties = {
       val props = new Properties()
       config.entrySet().foreach(entry => props.put(entry.getKey, entry.getValue.unwrapped().toString))
       props
     }
 
+    /**
+      * Convert Typesafe [[Config]] to a [[Map]].
+      */
     def toPropertyMap: Map[String, AnyRef] = {
       val map = mutable.Map[String, AnyRef]()
       config.entrySet().foreach(entry => map.put(entry.getKey, entry.getValue.unwrapped().toString))
