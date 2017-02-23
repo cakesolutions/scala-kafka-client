2512fe668048c61af950b0b1bfccad4a7a9468ee
Simon Souter <simonsouter@gmail.com>
2017-02-23 09:55:39 -0500

    Kafka0.10.2.0 (#98)
    * Update to Kafka 0.10.2.0
    
    * Remove staging repo from deps

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ object Dependencies {
     val logback = "1.1.7"
     val scalaTest = "3.0.0"
     val akka = "2.4.16"
-    val kafka = "0.10.1.1"
+    val kafka = "0.10.2.0"
     val typesafeConfig = "1.3.0"
   }
 }
