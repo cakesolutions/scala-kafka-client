4ed04cf7c9c1fc99df3e6d65981376a033f6dbd5
Simon Souter <simonsouter@gmail.com>
2017-03-02 14:24:50 +0000

    Update dependecies for next release (#100)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,10 +1,10 @@
 object Dependencies {
   object versions {
-    val slf4j = "1.7.21"
-    val logback = "1.1.7"
-    val scalaTest = "3.0.0"
-    val akka = "2.4.16"
+    val slf4j = "1.7.24"
+    val logback = "1.2.1"
+    val scalaTest = "3.0.1"
+    val akka = "2.4.17"
     val kafka = "0.10.2.0"
-    val typesafeConfig = "1.3.0"
+    val typesafeConfig = "1.3.1"
   }
 }
