a446d8d033a3e4535c1e738cc0b56ac230ddfc7a
Simon Souter <simonsouter@gmail.com>
2017-05-02 16:32:21 +0100

    Update kafka and akka deps (#110)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,8 +3,8 @@ object Dependencies {
     val slf4j = "1.7.24"
     val logback = "1.2.1"
     val scalaTest = "3.0.1"
-    val akka = "2.4.17"
-    val kafka = "0.10.2.0"
+    val akka = "2.4.18"
+    val kafka = "0.10.2.1"
     val typesafeConfig = "1.3.1"
   }
 }
