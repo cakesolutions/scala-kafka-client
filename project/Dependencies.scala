24a3a208b01ed41559b7cc39169af34da75ac0b7
Jimin Hsieh <jimin.hsieh.engineer@gmail.com>
2018-07-30 21:49:57 +0800

    Bump dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,9 +2,9 @@ object Dependencies {
   object versions {
     val slf4j = "1.7.25"
     val logback = "1.2.3"
-    val scalaTest = "3.0.4"
-    val akka = "2.5.12"
-    val kafka = "1.1.0"
+    val scalaTest = "3.0.5"
+    val akka = "2.5.14"
+    val kafka = "1.1.1"
     val typesafeConfig = "1.3.3"
   }
 }
