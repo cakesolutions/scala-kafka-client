c89b0686a434212087bf0318e07ccbe1bfc3733b
Regis Kuckaertz <75985301+regis-pirum@users.noreply.github.com>
2021-03-04 08:06:03 +0000

    Update akka version

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ object Dependencies {
     val slf4j = "1.7.29"
     val logback = "1.2.3"
     val scalaTest = "3.1.0"
-    val akka = "2.5.26"
+    val akka = "2.6.13"
     val kafka = "2.4.0"
     val typesafeConfig = "1.4.0"
   }
